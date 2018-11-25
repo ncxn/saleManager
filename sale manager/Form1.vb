6feeba9bfa585c2bcef19ed9b77f0b7316a4f414
ncxn <ncxn@outlook.com>
2018-11-25 12:44:11 +0700

    First commit

---
+++ sale manager/Form1.vb
@@ -0,0 +1,7 @@
+﻿
+Imports System.Windows.Forms
+Public Class Form1
+    Private Sub Form1_open(sender As Object, e As EventArgs) Handles MyBase.Load
+        MessageBox.Show(startPath)
+    End Sub
+End Class

0f0b28ce7528ebce6a7c8c041a6f238c79c80149
ncxn <ncxn@outlook.com>
2018-12-01 17:18:38 +0700

    3

--- sale manager/form/Product_type_list.Designer.vb
+++ sale manager/form/Product_type_list.Designer.vb
@@ -22,80 +22,19 @@ Partial Class Product_type_list
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.DataListView1 = New BrightIdeasSoftware.DataListView()
-        Me.OlvColumn1 = CType(New BrightIdeasSoftware.OLVColumn(), BrightIdeasSoftware.OLVColumn)
-        Me.OlvColumn2 = CType(New BrightIdeasSoftware.OLVColumn(), BrightIdeasSoftware.OLVColumn)
-        Me.OlvColumn3 = CType(New BrightIdeasSoftware.OLVColumn(), BrightIdeasSoftware.OLVColumn)
-        Me.OlvColumn4 = CType(New BrightIdeasSoftware.OLVColumn(), BrightIdeasSoftware.OLVColumn)
-        Me.OlvColumn5 = CType(New BrightIdeasSoftware.OLVColumn(), BrightIdeasSoftware.OLVColumn)
-        CType(Me.DataListView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Product_type_list))
         Me.SuspendLayout()
-        '
-        'DataListView1
-        '
-        Me.DataListView1.AllColumns.Add(Me.OlvColumn1)
-        Me.DataListView1.AllColumns.Add(Me.OlvColumn2)
-        Me.DataListView1.AllColumns.Add(Me.OlvColumn3)
-        Me.DataListView1.AllColumns.Add(Me.OlvColumn4)
-        Me.DataListView1.AllColumns.Add(Me.OlvColumn5)
-        Me.DataListView1.CellEditUseWholeCell = False
-        Me.DataListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.OlvColumn1, Me.OlvColumn2, Me.OlvColumn3, Me.OlvColumn4, Me.OlvColumn5})
-        Me.DataListView1.Cursor = System.Windows.Forms.Cursors.Default
-        Me.DataListView1.DataSource = Nothing
-        Me.DataListView1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.DataListView1.Location = New System.Drawing.Point(0, 0)
-        Me.DataListView1.Name = "DataListView1"
-        Me.DataListView1.Size = New System.Drawing.Size(800, 450)
-        Me.DataListView1.TabIndex = 0
-        Me.DataListView1.UseCompatibleStateImageBehavior = False
-        Me.DataListView1.View = System.Windows.Forms.View.Details
-        '
-        'OlvColumn1
-        '
-        Me.OlvColumn1.AspectName = "Product_Type_ID"
-        Me.OlvColumn1.Text = "ID"
-        '
-        'OlvColumn2
-        '
-        Me.OlvColumn2.AspectName = "Product_Type_Decscription"
-        Me.OlvColumn2.Text = "Nhóm Sản Phẩm"
-        Me.OlvColumn2.Width = 94
-        '
-        'OlvColumn3
-        '
-        Me.OlvColumn3.AspectName = "Product_Type_Parrent"
-        Me.OlvColumn3.Text = "Nhóm cha"
-        Me.OlvColumn3.Width = 99
-        '
-        'OlvColumn4
-        '
-        Me.OlvColumn4.AspectName = "Product_Type_Active"
-        Me.OlvColumn4.Text = "Sử dụng"
-        Me.OlvColumn4.Width = 116
-        '
-        'OlvColumn5
-        '
-        Me.OlvColumn5.AspectName = "Order"
-        Me.OlvColumn5.Text = "Sắp xếp"
-        Me.OlvColumn5.Width = 95
         '
         'Product_type_list
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.DataListView1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Product_type_list"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Nhóm sản phẩm"
-        CType(Me.DataListView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
-
-    Friend WithEvents DataListView1 As BrightIdeasSoftware.DataListView
-    Friend WithEvents OlvColumn1 As BrightIdeasSoftware.OLVColumn
-    Friend WithEvents OlvColumn2 As BrightIdeasSoftware.OLVColumn
-    Friend WithEvents OlvColumn3 As BrightIdeasSoftware.OLVColumn
-    Friend WithEvents OlvColumn4 As BrightIdeasSoftware.OLVColumn
-    Friend WithEvents OlvColumn5 As BrightIdeasSoftware.OLVColumn
 End Class
--- sale manager/form/Product_type_list.vb
+++ sale manager/form/Product_type_list.vb
@@ -1,8 +1,5 @@
 ﻿Imports BrightIdeasSoftware
 Public Class Product_type_list
-    Private Sub ObjectListView_Product_type_SelectedIndexChanged(sender As Object, e As EventArgs)
-
-    End Sub
 
     Private Sub Product_type_list_Load(sender As Object, e As EventArgs) Handles Me.Load
         Dim db As New SQLiteDatabase
@@ -10,11 +7,7 @@ Public Class Product_type_list
         Dim strSQL As String
         strSQL = "SELECT * FROM Product_Type"
         tb = db.GetDataTable(strSQL)
-        DataListView1.DataSource = tb
 
     End Sub
 
-    Private Sub DataListView1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DataListView1.SelectedIndexChanged
-
-    End Sub
 End Class
